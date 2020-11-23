918940aedd4c9bfdddbd17c806968ef313c43a8f
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-11-23 16:17:13 -0500

    Add SystemExecutioner (#3)
    * Add SystemExecutioner
    * Add exclusion for sonar code coverage since we can't actually test this class
    
    Closes #2

---
+++ src/main/java/org/kiwiproject/dropwizard/util/startup/SystemExecutioner.java
@@ -0,0 +1,17 @@
+package org.kiwiproject.dropwizard.util.startup;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Wrapper around {@link System#exit(int)} to allow for unit testing scenarios in which we need to exit the JVM, for
+ * example when the Jetty server cannot start due to failure to obtain a port, or some other non-recoverable startup
+ * error.
+ */
+@Slf4j
+public class SystemExecutioner {
+
+    void exit() {
+        LOG.warn("Terminating the VM!");
+        System.exit(1);
+    }
+}
