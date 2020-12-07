74c38d4088ae858b735537dbdf66ff930d0d1a3b
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-12-07 10:46:19 -0500

    Add ConfigResource (#33)
    Closes #31

---
+++ src/main/java/org/kiwiproject/dropwizard/util/resource/ConfigResource.java
@@ -0,0 +1,46 @@
+package org.kiwiproject.dropwizard.util.resource;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.kiwiproject.json.KiwiJacksonSerializers.buildPropertyMaskingSafeSerializerModule;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
+import io.dropwizard.Configuration;
+import lombok.extern.slf4j.Slf4j;
+import org.kiwiproject.json.JsonHelper;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+/**
+ * JAX-RS resource that enables the retrieval of the current runtime configuration of the service.
+ * <p>
+ * A list of regex patterns can be given in order to mask secrets in the configuration.
+ */
+@Slf4j
+@Path("app/config")
+public class ConfigResource<T extends Configuration> {
+
+    // Intentionally creating a separate JsonHelper to allow customizations
+    private final JsonHelper jsonHelper;
+    private final T config;
+
+    public ConfigResource(T config, List<String> hiddenRegex) {
+        this.config = config;
+        this.jsonHelper = JsonHelper.newDropwizardJsonHelper();
+
+        jsonHelper.getObjectMapper().registerModule(buildPropertyMaskingSafeSerializerModule(hiddenRegex));
+    }
+
+    @GET
+    @Timed
+    @ExceptionMetered
+    @Produces(APPLICATION_JSON)
+    public Response getCurrentConfiguration() {
+        var json = jsonHelper.toJson(config);
+        return Response.ok(json).build();
+    }
+}
