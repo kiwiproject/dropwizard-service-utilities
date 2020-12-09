a7bdfb0a12babf7dd14949cf21f820aecc3908fb
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-12-09 17:11:33 -0500

    Add MongoHealthCheck (#52)
    * Add MongoHealthCheck
    
    Closes #42

---
+++ src/main/java/org/kiwiproject/dropwizard/util/health/MongoHealthCheck.java
@@ -0,0 +1,56 @@
+package org.kiwiproject.dropwizard.util.health;
+
+import static org.kiwiproject.metrics.health.HealthCheckResults.newResultBuilder;
+import static org.kiwiproject.metrics.health.HealthCheckResults.newUnhealthyResult;
+
+import com.codahale.metrics.health.HealthCheck;
+import com.mongodb.DB;
+import lombok.extern.slf4j.Slf4j;
+import org.kiwiproject.metrics.health.HealthStatus;
+
+/**
+ * Health check that attempts to pull dbStats from Mongo and report on the status.
+ * <p>
+ * Note this health check is currently using the deprecated {@link DB} class.
+ */
+@Slf4j
+public class MongoHealthCheck extends HealthCheck {
+
+    public static final String TITLE = "Mongo";
+
+    private final DB db;
+
+    public MongoHealthCheck(DB db) {
+        this.db = db;
+    }
+
+
+    @SuppressWarnings({"deprecation", "ConstantConditions"})
+    @Override
+    protected Result check() throws Exception {
+        var host = db.getMongo().getAddress().toString();
+        var dbName = db.getName();
+
+        try {
+            var statsResult = db.getStats();
+
+            var isHealthy = statsResult.ok();
+            var resultBuilder = newResultBuilder(isHealthy)
+                    .withDetail("storageSize", statsResult.get("storageSize"))
+                    .withDetail("dataSize", statsResult.get("dataSize"));
+
+            if (isHealthy) {
+                return resultBuilder.withMessage("Mongo %s/%s is up", host, dbName).build();
+            }
+
+            return resultBuilder
+                    .withMessage("Failed to retrieve db stats %s/%s : %s",
+                            host, dbName, statsResult.getErrorMessage())
+                    .build();
+        } catch (Exception e) {
+            LOG.error("Unable to connect to Mongo: {}/{}", host, dbName, e);
+            return newUnhealthyResult(HealthStatus.CRITICAL, "Unable to connect to Mongo %s/%s: %s",
+                    host, dbName, e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/org/kiwiproject/dropwizard/util/health/MongoHealthCheckTest.java
@@ -0,0 +1,93 @@
+package org.kiwiproject.dropwizard.util.health;
+
+import static org.kiwiproject.test.assertj.dropwizard.metrics.HealthCheckResultAssertions.assertThatHealthCheck;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.mongodb.CommandResult;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.ServerAddress;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.kiwiproject.metrics.health.HealthStatus;
+
+@SuppressWarnings("deprecation")
+@DisplayName("MongoHealthCheck")
+class MongoHealthCheckTest {
+
+    private static final String DB_NAME = "someName";
+    private static final String SERVER_ADDRESS = "localhost:27017";
+
+    @Nested
+    class IsHealthy {
+
+        @Test
+        void whenGetStatsReturnsSuccessfully() {
+            var mockDb = createMockedMongoDb(true);
+            var healthCheck = new MongoHealthCheck(mockDb);
+
+            assertThatHealthCheck(healthCheck)
+                    .isHealthy()
+                    .hasMessage("Mongo " + SERVER_ADDRESS + "/" + DB_NAME + " is up")
+                    .hasDetailsContainingKeys("storageSize", "dataSize");
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Nested
+    class IsUnhealthy {
+
+        @Test
+        void whenExceptionIsThrown() {
+            var mockDb = setupMockDB();
+            when(mockDb.getStats()).thenThrow(new RuntimeException("oops"));
+
+            var healthCheck = new MongoHealthCheck(mockDb);
+
+            assertThatHealthCheck(healthCheck)
+                    .isUnhealthy()
+                    .hasDetail("severity", HealthStatus.CRITICAL.name())
+                    .hasMessageStartingWith("Unable to connect to Mongo " + SERVER_ADDRESS + "/" + DB_NAME + ": oops");
+
+        }
+
+        @Test
+        void whenGetStatsFails() {
+            var mockDb = createMockedMongoDb(false);
+            var healthCheck = new MongoHealthCheck(mockDb);
+
+            assertThatHealthCheck(healthCheck)
+                    .isUnhealthy()
+                    .hasMessageStartingWith("Failed to retrieve db stats " + SERVER_ADDRESS + "/" + DB_NAME + " : ");
+        }
+    }
+
+    private static DB createMockedMongoDb(boolean getStatsSuccessful) {
+        var mockDb = setupMockDB();
+        var mockedResult = mockedCommandResult(getStatsSuccessful);
+
+        when(mockDb.getStats()).thenReturn(mockedResult);
+        return mockDb;
+    }
+
+    private static DB setupMockDB() {
+        var mockDb = mock(DB.class);
+        var mockMongo = mock(Mongo.class);
+        var mockServerAddress = mock(ServerAddress.class);
+
+        when(mockDb.getMongo()).thenReturn(mockMongo);
+        when(mockMongo.getAddress()).thenReturn(mockServerAddress);
+        when(mockServerAddress.toString()).thenReturn(MongoHealthCheckTest.SERVER_ADDRESS);
+        when(mockDb.getName()).thenReturn(MongoHealthCheckTest.DB_NAME);
+
+        return mockDb;
+    }
+
+    private static CommandResult mockedCommandResult(boolean ok) {
+        var result = mock(CommandResult.class);
+        when(result.ok()).thenReturn(ok);
+        return result;
+    }
+}
