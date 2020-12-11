daa1cc1aaf9d3a2e1c17aa09a70108fdb790f56e
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-12-11 15:51:50 -0500

    Added more javadocs

--- src/main/java/org/kiwiproject/dropwizard/util/config/JobSchedule.java
+++ src/main/java/org/kiwiproject/dropwizard/util/config/JobSchedule.java
@@ -17,9 +17,18 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class JobSchedule {
 
+    /**
+     * Initial delay before the job will run. This should be passed to the ScheduledExecutor's initialDelay. Defaults
+     * to 30 seconds.
+     */
     @NotNull
     private Duration initialDelay = Duration.seconds(30);
 
+    /**
+     * Delay between job runs.  If using a fixedDelay, this will be the time between the end of one job run and the
+     * start of the next job run. If using a fixedRate, this will be the time between the start of one job and the start
+     * of the next run.
+     */
     @NotNull
     private Duration intervalDelay;
 
