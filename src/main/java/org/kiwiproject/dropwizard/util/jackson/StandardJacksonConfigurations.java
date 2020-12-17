7f8e2ac3bedcb1e844dd4e6bd3d6aa8c947760ee
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-12-17 17:06:41 -0500

    Enhance StandardJacksonConfigurations javadocs
    * Add additional explanation to registerAllStandardJacksonConfigurations
      documentation in StandardJacksonConfigurations
    * IntelliJ also decided to reformat some of the Javadocs, etc. when I
      reformatted the class

--- src/main/java/org/kiwiproject/dropwizard/util/jackson/StandardJacksonConfigurations.java
+++ src/main/java/org/kiwiproject/dropwizard/util/jackson/StandardJacksonConfigurations.java
@@ -22,11 +22,15 @@ import javax.xml.bind.JAXBElement;
 public class StandardJacksonConfigurations {
 
     /**
-     * Registers all of the "standard" Jackson configurations.
+     * Registers all of the "standard" Jackson configurations on the {@link ObjectMapper} contained in the given
+     * Dropwizard {@link Environment} according to the values in the {@link JacksonConfig}.
+     * <p>
+     * Please note that this <em>modifies</em> the Dropwizard {@link Environment}'s object mapper with our "standard"
+     * configurations.
      *
-     * @param config        a config model containing various options for configuring Jackson
-     * @param environment   the Dropwizard environment
-     * @return The configured {@link ObjectMapper}
+     * @param config      a config model containing various options for configuring Jackson
+     * @param environment the Dropwizard environment
+     * @return The configured {@link ObjectMapper}, which is the same exact object returned by {@link Environment#getObjectMapper()})
      * @see StandardJacksonConfigurations#registerJacksonDeserializationProblemHandler(JacksonConfig, ObjectMapper, HealthCheckRegistry)
      * @see StandardJacksonConfigurations#registerJacksonTimestampSerialization(JacksonConfig, ObjectMapper)
      * @see StandardJacksonConfigurations#registerJaxbSerializer(JacksonConfig, ObjectMapper)
@@ -46,13 +50,13 @@ public class StandardJacksonConfigurations {
      * method will also optionally register a Health Check to mark the service unhealthy if there are deserialization
      * issues.
      *
-     * @param config        a config model containing various options for configuring Jackson
-     * @param mapper        the {@link ObjectMapper} to attach the handler to
-     * @param healthChecks  the {@link HealthCheckRegistry} to register the health check on
+     * @param config       a config model containing various options for configuring Jackson
+     * @param mapper       the {@link ObjectMapper} to attach the handler to
+     * @param healthChecks the {@link HealthCheckRegistry} to register the health check on
      */
     public static void registerJacksonDeserializationProblemHandler(JacksonConfig config,
-                                                                     ObjectMapper mapper,
-                                                                     HealthCheckRegistry healthChecks) {
+                                                                    ObjectMapper mapper,
+                                                                    HealthCheckRegistry healthChecks) {
 
         if (config.isIgnoreButWarnForUnknownJsonProperties()) {
             var handler = new LoggingDeserializationProblemHandler();
@@ -67,8 +71,8 @@ public class StandardJacksonConfigurations {
     /**
      * Optionally configures the {@link ObjectMapper} to convert all Timestamps into milliseconds.
      *
-     * @param config        a config model containing various options for configuring Jackson
-     * @param mapper        the {@link ObjectMapper} to configure
+     * @param config a config model containing various options for configuring Jackson
+     * @param mapper the {@link ObjectMapper} to configure
      */
     public static void registerJacksonTimestampSerialization(JacksonConfig config, ObjectMapper mapper) {
         if (config.isReadAndWriteDateTimestampsAsMillis()) {
@@ -80,8 +84,8 @@ public class StandardJacksonConfigurations {
     /**
      * Optionally configures the {@link ObjectMapper} to convert all nil {@link JAXBElement} into null.
      *
-     * @param config        a config model containing various options for configuring Jackson
-     * @param mapper        the {@link ObjectMapper} to configure
+     * @param config a config model containing various options for configuring Jackson
+     * @param mapper the {@link ObjectMapper} to configure
      */
     public static void registerJaxbSerializer(JacksonConfig config, ObjectMapper mapper) {
         if (config.isWriteNilJaxbElementsAsNull()) {
