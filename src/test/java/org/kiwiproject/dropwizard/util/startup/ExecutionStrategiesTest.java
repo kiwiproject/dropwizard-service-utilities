62b8166f6bc827cb355b77713a67beacffa59660
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2021-01-19 13:01:14 -0500

    Introduce ExecutionStrategy as pluggable strategy to SystemExecutioner
    * Introduce ExecutionStrategy interface as a pluggable strategy for
      terminating the JVM (or not terminating in tests, etc.)
    * ExecutionStrategies is a factory class that provides several
      strategies: a no-op strategy, an exit "flagging" strategy, and a
      System.exit strategy.
    * SystemExecutioner remains backward-compatible since the no-args
      constructor uses the System.exit strategy but provides a way
      to construct an instance with some other ExecutionStrategy. This will
      mainly be useful in testing situations.
    * Note also that even though Jacoco will say the exit() method of
      SystemExitExecutionStrategy is not covered by tests, it actually *is*
      covered because ExecutionStrategiesTest actually launches a separate
      JVM and verifies that it was exited.

---
+++ src/test/java/org/kiwiproject/dropwizard/util/startup/ExecutionStrategiesTest.java
@@ -0,0 +1,128 @@
+package org.kiwiproject.dropwizard.util.startup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.kiwiproject.test.assertj.KiwiAssertJ.assertIsExactType;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.kiwiproject.base.process.Processes;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@DisplayName("ExecutionStrategies")
+class ExecutionStrategiesTest {
+
+    @Test
+    void shouldBuildNoOpStrategy() {
+        assertThat(ExecutionStrategies.noOp())
+                .isExactlyInstanceOf(ExecutionStrategies.NoOpExecutionStrategy.class);
+    }
+
+    @Test
+    void shouldBuildSystemExitStrategy() {
+        var strategy = ExecutionStrategies.systemExit();
+        var systemExitStrategy = assertIsExactType(strategy, ExecutionStrategies.SystemExitExecutionStrategy.class);
+        assertThat(systemExitStrategy.getExitCode()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldBuildSystemExitStrategyWithExitCode() {
+        var strategy = ExecutionStrategies.systemExit(42);
+        var systemExitStrategy = assertIsExactType(strategy, ExecutionStrategies.SystemExitExecutionStrategy.class);
+        assertThat(systemExitStrategy.getExitCode()).isEqualTo(42);
+    }
+
+    @Nested
+    class NoOpStrategy {
+
+        @Test
+        void shouldDoNothing() {
+            var executionStrategy = ExecutionStrategies.noOp();
+            assertThatCode(executionStrategy::exit).doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    class ExitFlaggingStrategy {
+
+        @Test
+        void shouldFlagCallsToExit() {
+            var executionStrategy = ExecutionStrategies.exitFlagging();
+            assertThatCode(executionStrategy::exit).doesNotThrowAnyException();
+
+            var exitFlaggingStrategy =
+                    assertIsExactType(executionStrategy, ExecutionStrategies.ExitFlaggingExecutionStrategy.class);
+            assertThat(exitFlaggingStrategy.didExit()).isTrue();
+        }
+
+        @Test
+        void shouldNotFlagWhenExitNotCalled() {
+            var exitFlaggingStrategy =
+                    assertIsExactType(ExecutionStrategies.exitFlagging(), ExecutionStrategies.ExitFlaggingExecutionStrategy.class);
+            assertThat(exitFlaggingStrategy.didExit()).isFalse();
+        }
+    }
+
+    @Nested
+    class SystemExitStrategy {
+
+        @ParameterizedTest
+        @CsvSource({
+                "1, 0",
+                "143 , 0",
+                "127, 25",
+                "143, 50"
+        })
+        void shouldExitTheJVM(int exitCode, long exitWaitDelayMillis) throws IOException {
+            var result = execTestApplication(exitCode, exitWaitDelayMillis);
+
+            assertThat(result.pid).isPositive();
+            assertThat(result.exitValue).isEqualTo(exitCode);
+        }
+    }
+
+    @Slf4j
+    public static class TestApplication {
+        public static void main(String[] args) {
+            int exitCode = Integer.parseInt(args[0]);
+            long exitWaitDelayMillis = Long.parseLong(args[1]);
+            LOG.debug("Using exitCode: {} ; exitWaitDelayMillis: {}", exitCode, exitWaitDelayMillis);
+
+            // pretend some unrecoverable error occurred...
+            var executionStrategy = ExecutionStrategies.systemExit(exitCode);
+            new SystemExecutioner(executionStrategy).exit(exitWaitDelayMillis, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static ExecResult execTestApplication(int exitCode, long exitWaitDelayMillis) throws IOException {
+        var javaHome = System.getProperty("java.home");
+        var javaBin = Path.of(javaHome, "bin", "java").toString();
+        var classpath = System.getProperty("java.class.path");
+        var className = TestApplication.class.getName();
+        var command = List.of(javaBin,
+                "-cp", classpath,
+                className,
+                String.valueOf(exitCode),
+                String.valueOf(exitWaitDelayMillis));
+
+        var process = new ProcessBuilder(command).start();
+        var exitValueOrNull = Processes.waitForExit(process, 1, TimeUnit.SECONDS).orElse(null);
+        return new ExecResult(process.pid(), exitValueOrNull);
+    }
+
+    @Value
+    private static class ExecResult {
+        long pid;
+        Integer exitValue;
+    }
+}
---
+++ src/test/java/org/kiwiproject/dropwizard/util/startup/SystemExecutionerTest.java
@@ -0,0 +1,110 @@
+package org.kiwiproject.dropwizard.util.startup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Durations.ONE_SECOND;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.kiwiproject.base.DefaultEnvironment;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+@DisplayName("SystemExecutioner")
+@Slf4j
+class SystemExecutionerTest {
+
+    @Test
+    void shouldUseSystemExitStrategyByDefault() {
+        var executioner = new SystemExecutioner();
+        assertThat(executioner.getExecutionStrategy())
+                .isExactlyInstanceOf(ExecutionStrategies.SystemExitExecutionStrategy.class);
+    }
+
+    @Test
+    void shouldExitImmediately() {
+        var executionStrategy = new ExecutionStrategies.ExitFlaggingExecutionStrategy();
+        var executioner = new SystemExecutioner(executionStrategy);
+        long startTime = System.nanoTime();
+        executioner.exit();
+        var elapsedNanos = System.nanoTime() - startTime;
+
+        assertThat(executionStrategy.didExit()).isTrue();
+
+        long elapsedMillis = TimeUnit.NANOSECONDS.toMillis(elapsedNanos);
+        LOG.info("elapsedMillis: {} (elapsedNanos: {})", elapsedMillis, elapsedNanos);
+        assertThat(elapsedMillis).isZero();
+    }
+
+    @Test
+    void shouldExitWithWaitTime() {
+        var executorService = Executors.newSingleThreadExecutor();
+
+        var waitTimeMillis = 25;
+        var executionStrategy = new ExecutionStrategies.ExitFlaggingExecutionStrategy();
+        var executioner = new SystemExecutioner(executionStrategy);
+        var startTime = new AtomicLong();
+
+        var executionFuture = executorService.submit(() -> {
+            LOG.info("Calling executioner...");
+            startTime.set(System.nanoTime());
+            executioner.exit(waitTimeMillis, TimeUnit.MILLISECONDS);
+        });
+
+        await().atMost(ONE_SECOND).until(executionFuture::isDone);
+
+        long elapsedNanos = System.nanoTime() - startTime.get();
+
+        assertThat(executionStrategy.didExit())
+                .describedAs("Execution strategy exit() should have been called")
+                .isTrue();
+
+        assertThat(TimeUnit.NANOSECONDS.toMillis(elapsedNanos))
+                .describedAs("Elapsed millis must be greater than %d", waitTimeMillis)
+                .isGreaterThan(waitTimeMillis);
+
+        executorService.shutdown();
+        await().atMost(ONE_SECOND).until(executorService::isShutdown);
+    }
+
+    @Test
+    void shouldExitBeforeGivenWaitTime_WhenWaitingThreadInterrupted() {
+        var executorService = Executors.newFixedThreadPool(2);
+
+        var executionStrategy = new ExecutionStrategies.ExitFlaggingExecutionStrategy();
+        var executioner = new SystemExecutioner(executionStrategy);
+        var startTime = new AtomicLong();
+        var executionFuture = executorService.submit(() -> {
+            LOG.info("Calling executioner with 5 second wait");
+            startTime.set(System.nanoTime());
+            executioner.exit(5, TimeUnit.SECONDS);
+        });
+
+        var killerSleepTimeMillis = 100;
+        var killerFuture = executorService.submit(() -> {
+            LOG.info("Sleeping for {} milliseconds...", killerSleepTimeMillis);
+            new DefaultEnvironment().sleepQuietly(killerSleepTimeMillis, TimeUnit.MILLISECONDS);
+            LOG.info("I'm awake and will now interrupt executionThread");
+            var canceled = executionFuture.cancel(true);
+            LOG.info("executionFuture was canceled? {}", canceled);
+        });
+
+        await().atMost(ONE_SECOND).until(() -> executionFuture.isDone() && killerFuture.isDone());
+
+        long elapsedNanos = System.nanoTime() - startTime.get();
+
+        assertThat(executionStrategy.didExit())
+                .describedAs("Execution strategy exit() should have been called")
+                .isTrue();
+
+        assertThat(TimeUnit.NANOSECONDS.toMillis(elapsedNanos))
+                .describedAs("Elapsed millis must be greater than %d", killerSleepTimeMillis)
+                .isGreaterThan(killerSleepTimeMillis);
+
+        executorService.shutdown();
+        await().atMost(ONE_SECOND).until(executorService::isShutdown);
+    }
+}
