1e57135a4aa35c5b99bc6a98aa6f936886670f60
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-04-11 17:20:21 -0400

    Replace deprecated AbstractLifeCycleListener with LifeCycle.Listener
    * Replace deprecated AbstractLifeCycle.AbstractLifeCycleListener usages
      by implementing LifeCycle.Listener directly (it now has default
      methods so there's no need for the abstract class anymore)
    
    Closes #221

--- src/main/java/org/kiwiproject/dropwizard/util/startup/listener/StartupJettyLifeCycleListener.java
+++ src/main/java/org/kiwiproject/dropwizard/util/startup/listener/StartupJettyLifeCycleListener.java
@@ -3,16 +3,15 @@ package org.kiwiproject.dropwizard.util.startup.listener;
 import static org.kiwiproject.base.KiwiPreconditions.requireNotNull;
 
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.jetty.util.component.AbstractLifeCycle;
 import org.eclipse.jetty.util.component.LifeCycle;
 import org.kiwiproject.dropwizard.util.startup.SystemExecutioner;
 
 /**
- * A Jetty {@link org.eclipse.jetty.util.component.LifeCycle.Listener} that shuts down the system in the case of
+ * A Jetty {@link LifeCycle.Listener} that shuts down the system in the case of
  * a lifecycle failure.
  */
 @Slf4j
-public class StartupJettyLifeCycleListener extends AbstractLifeCycle.AbstractLifeCycleListener {
+public class StartupJettyLifeCycleListener implements LifeCycle.Listener {
 
     private final SystemExecutioner executioner;
 
