e9b576b183aa5f755143fe8307ed29def8ad4bc8
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-04-19 20:53:45 +0000

    Change MongoHealthCheck to work with 4.x driver
    This is a breaking API change, since the DB class no longer exists
    in the Mongo 4.x driver. It was  replaced by MongoDatabase. Thus,
    the constructor had to change. The MongoDatabase also does not
    contain as much information, e.g. you cannot obtain the host and port
    information as you could with DB, which provided a getMongo() method
    from which you could obtain a ServerAddress in order to find the host
    and port. We decided that information does not add enough value to
    require callers to include a host and port, though we could re-evauate
    that decision later.
    
    This commit, therefore, changes MongoHealthCheck to work with the
    4.x driver. The health check has been using the "dbStats" command which
    "returns storage statistics for a given database". The problem with this
    is that, from the docs, "The time required to run the command depends on
    the total size of the database. Because the command must touch all data
    files, the command may take several seconds to run." Health checks
    should definitely not run any commands that could cause performance
    problems or take seconds to execute.
    
    This commit changes the MongoHealthCheck to use the ping command, which
    "is a no-op used to test whether a server is responding to commands",
    and it "will return immediately even if the server is write-locked". This
    is much better for a health check, and should obviously be much faster and
    less resource intensive on Mongo.
    
    Closes #233

--- src/test/java/org/kiwiproject/dropwizard/util/health/MongoHealthCheckTest.java
+++ src/test/java/org/kiwiproject/dropwizard/util/health/MongoHealthCheckTest.java
@@ -1,93 +1,154 @@
 package org.kiwiproject.dropwizard.util.health;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kiwiproject.base.KiwiStrings.f;
 import static org.kiwiproject.test.assertj.dropwizard.metrics.HealthCheckResultAssertions.assertThatHealthCheck;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.mongodb.CommandResult;
-import com.mongodb.DB;
-import com.mongodb.Mongo;
-import com.mongodb.ServerAddress;
+import java.util.List;
+import java.util.Optional;
+
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.BsonDocument;
+import org.bson.BsonInt32;
+import org.bson.Document;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.kiwiproject.collect.KiwiMaps;
 import org.kiwiproject.metrics.health.HealthStatus;
+import org.mockito.ArgumentCaptor;
+import org.mockito.stubbing.OngoingStubbing;
 
-@SuppressWarnings("deprecation")
 @DisplayName("MongoHealthCheck")
 class MongoHealthCheckTest {
 
-    private static final String DB_NAME = "someName";
-    private static final String SERVER_ADDRESS = "localhost:27017";
+    private static final String DB_NAME = "testDatabase";
+    private static final String OK_PROPERTY = "ok";
+    private static final String ERROR_MESSAGE_PROPERTY = "errmsg";
+    private static final String CODE_PROPERTY = "code";
+    private static final String CODE_NAME_PROPERTY = "codeName";
 
     @Nested
     class IsHealthy {
 
-        @Test
-        void whenGetStatsReturnsSuccessfully() {
-            var mockDb = createMockedMongoDb(true);
-            var healthCheck = new MongoHealthCheck(mockDb);
+        @ParameterizedTest
+        @MethodSource("org.kiwiproject.dropwizard.util.health.MongoHealthCheckTest#successfulCommandResults")
+        void whenPingSucceeds(Document commandResult) {
+            var database = setupMockMongoDatabase();
+            whenReceivesRunCommand(database).thenReturn(commandResult);
+
+            var healthCheck = new MongoHealthCheck(database);
 
             assertThatHealthCheck(healthCheck)
                     .isHealthy()
-                    .hasMessage("Mongo " + SERVER_ADDRESS + "/" + DB_NAME + " is up")
-                    .hasDetailsContainingKeys("storageSize", "dataSize");
+                    .hasMessage(f("Successfully pinged Mongo database {}", DB_NAME))
+                    .hasDetail("severity", HealthStatus.OK.name())
+                    .hasDetail(OK_PROPERTY, commandResult.get(OK_PROPERTY))
+                    .doesNotHaveDetailsContainingKeys(ERROR_MESSAGE_PROPERTY, CODE_PROPERTY, CODE_NAME_PROPERTY);
+
+            verifyRunPingCommand(database);
         }
     }
 
-    @SuppressWarnings("deprecation")
     @Nested
     class IsUnhealthy {
 
         @Test
         void whenExceptionIsThrown() {
-            var mockDb = setupMockDB();
-            when(mockDb.getStats()).thenThrow(new RuntimeException("oops"));
+            var database = setupMockMongoDatabase();
+            var message = "oops";
+            whenReceivesRunCommand(database).thenThrow(new RuntimeException(message));
 
-            var healthCheck = new MongoHealthCheck(mockDb);
+            var healthCheck = new MongoHealthCheck(database);
 
             assertThatHealthCheck(healthCheck)
                     .isUnhealthy()
+                    .hasMessage(f("Error pinging Mongo database {} ({}: {})",
+                            DB_NAME, RuntimeException.class.getName(), message))
                     .hasDetail("severity", HealthStatus.CRITICAL.name())
-                    .hasMessageStartingWith("Unable to connect to Mongo " + SERVER_ADDRESS + "/" + DB_NAME + ": oops");
+                    .doesNotHaveDetailsContainingKeys(OK_PROPERTY, ERROR_MESSAGE_PROPERTY, CODE_PROPERTY, CODE_NAME_PROPERTY);
 
+            verifyRunPingCommand(database);
         }
 
-        @Test
-        void whenGetStatsFails() {
-            var mockDb = createMockedMongoDb(false);
-            var healthCheck = new MongoHealthCheck(mockDb);
+        @ParameterizedTest
+        @MethodSource("org.kiwiproject.dropwizard.util.health.MongoHealthCheckTest#failedCommandResults")
+        void whenCommandFails(Document commandResult) {
+            var database = setupMockMongoDatabase();
+            whenReceivesRunCommand(database).thenReturn(commandResult);
+
+            var healthCheck = new MongoHealthCheck(database);
+
+            var expectedErrmsgValue = Optional.ofNullable(commandResult.get(ERROR_MESSAGE_PROPERTY))
+                    .orElse("[No errmsg value]");
 
             assertThatHealthCheck(healthCheck)
                     .isUnhealthy()
-                    .hasMessageStartingWith("Failed to retrieve db stats " + SERVER_ADDRESS + "/" + DB_NAME + " : ");
+                    .hasMessage(f("Error pinging Mongo database {} : {}", DB_NAME, expectedErrmsgValue))
+                    .hasDetail("severity", HealthStatus.WARN.name())
+                    .hasDetail(OK_PROPERTY, commandResult.get(OK_PROPERTY))
+                    .hasDetail(CODE_PROPERTY, commandResult.get(CODE_PROPERTY))
+                    .hasDetail(CODE_NAME_PROPERTY, commandResult.get(CODE_NAME_PROPERTY));
+
+            verifyRunPingCommand(database);
         }
     }
 
-    private static DB createMockedMongoDb(boolean getStatsSuccessful) {
-        var mockDb = setupMockDB();
-        var mockedResult = mockedCommandResult(getStatsSuccessful);
-
-        when(mockDb.getStats()).thenReturn(mockedResult);
-        return mockDb;
+    private static MongoDatabase setupMockMongoDatabase() {
+        var mockDatabase = mock(MongoDatabase.class);
+        when(mockDatabase.getName()).thenReturn(DB_NAME);
+        return mockDatabase;
     }
 
-    private static DB setupMockDB() {
-        var mockDb = mock(DB.class);
-        var mockMongo = mock(Mongo.class);
-        var mockServerAddress = mock(ServerAddress.class);
-
-        when(mockDb.getMongo()).thenReturn(mockMongo);
-        when(mockMongo.getAddress()).thenReturn(mockServerAddress);
-        when(mockServerAddress.toString()).thenReturn(MongoHealthCheckTest.SERVER_ADDRESS);
-        when(mockDb.getName()).thenReturn(MongoHealthCheckTest.DB_NAME);
-
-        return mockDb;
+    private OngoingStubbing<Document> whenReceivesRunCommand(MongoDatabase database) {
+        return when(database.runCommand(any(BsonDocument.class)));
     }
 
-    private static CommandResult mockedCommandResult(boolean ok) {
-        var result = mock(CommandResult.class);
-        when(result.ok()).thenReturn(ok);
-        return result;
+    private static List<Document> successfulCommandResults() {
+        var ok = OK_PROPERTY;
+        return List.of(
+            new Document(ok, true),
+            new Document(ok, 1),
+            new Document(ok, 1L),
+            new Document(ok, 1.0F),
+            new Document(ok, 1.0)
+        );
+    }
+
+    private static List<Document> failedCommandResults() {
+        return List.of(
+            newFailedDocument("no such command 'crash'", 59, "CommandNotFound"),
+            newFailedDocument("scale has to be a number > 0", null, null),
+            newFailedDocument("an error occurred", 42, "The Code", false),
+            newFailedDocument("unknown error", null, null, null),
+            newFailedDocument(null, null, null, "true")
+        );
+    }
+
+    private static Document newFailedDocument(String errmsg, Integer code, String codeName) {
+        return newFailedDocument(errmsg, code, codeName, 0.0);
+    }
+
+    private static Document newFailedDocument(String errmsg, Integer code, String codeName, Object okValue) {
+        return new Document(KiwiMaps.newHashMap(
+            OK_PROPERTY, okValue,
+            ERROR_MESSAGE_PROPERTY, errmsg,
+            CODE_PROPERTY, code,
+            CODE_NAME_PROPERTY, codeName
+        ));
+    }
+
+    private void verifyRunPingCommand(MongoDatabase database) {
+        var inputCaptor = ArgumentCaptor.forClass(BsonDocument.class);
+        verify(database).runCommand(inputCaptor.capture());
+        var bsonInputDoc = inputCaptor.getValue();
+        assertThat(bsonInputDoc).containsEntry("ping", new BsonInt32(1));
     }
 }
