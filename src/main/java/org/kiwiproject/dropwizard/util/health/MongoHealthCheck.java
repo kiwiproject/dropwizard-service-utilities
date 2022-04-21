d2b3bb282ae96b28241e5558e64e9fecc22d8760
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-04-21 13:31:35 -0400

    Change MongoHealthCheck to work with 4.x driver (#235)
    * Change MongoHealthCheck to work with 4.x driver
    
    This is a breaking API change, since the DB class no longer exists
    in the Mongo 4.x driver. It was  replaced by MongoDatabase. Thus,
    the constructor had to change. The MongoDatabase also does not
    contain as much information, e.g. you cannot obtain the host and port
    information as you could with DB, which provided a getMongo() method
    from which you could obtain a ServerAddress in order to find the host
    and port. We decided that information does not add enough value to
    require callers to include a host and port, though we could re-evauate
    that decision later.
    
    This commit, therefore, changes MongoHealthCheck to work with the
    4.x driver. The health check has been using the "dbStats" command which
    "returns storage statistics for a given database". The problem with this
    is that, from the docs, "The time required to run the command depends on
    the total size of the database. Because the command must touch all data
    files, the command may take several seconds to run." Health checks
    should definitely not run any commands that could cause performance
    problems or take seconds to execute.
    
    This commit changes the MongoHealthCheck to use the ping command, which
    "is a no-op used to test whether a server is responding to commands",
    and it "will return immediately even if the server is write-locked". This
    is much better for a health check, and should obviously be much faster and
    less resource intensive on Mongo.
    
    Closes #233
    
    * Bump mongo driver versions to 4.6.0
    
    * Bump mongodb-driver-core from 4.5.1 to 4.6.0
    * Bump mongodb-driver-sync from 4.5.1 to 4.6.0
    
    * REVERT: Bump mongo driver versions to 4.6.0
    
    * Update kiwi-parent and kiwi-bom versions

--- src/main/java/org/kiwiproject/dropwizard/util/health/MongoHealthCheck.java
+++ src/main/java/org/kiwiproject/dropwizard/util/health/MongoHealthCheck.java
@@ -1,20 +1,29 @@
 package org.kiwiproject.dropwizard.util.health;
 
+import static org.kiwiproject.base.KiwiStrings.f;
 import static org.kiwiproject.metrics.health.HealthCheckResults.newResultBuilder;
 import static org.kiwiproject.metrics.health.HealthCheckResults.newUnhealthyResult;
 
+import java.util.Optional;
+
 import com.codahale.metrics.health.HealthCheck;
-import com.mongodb.DB;
+import com.mongodb.client.MongoDatabase;
+
 import lombok.extern.slf4j.Slf4j;
-import org.kiwiproject.metrics.health.HealthStatus;
+
+import org.bson.BsonDocument;
+import org.bson.BsonInt32;
+import org.bson.Document;
 
 /**
- * Health check that attempts to pull dbStats from Mongo and report on the status.
- * <p>
- * Note this health check is currently using the deprecated {@link DB} class.
+ * Health check that attempts to issue a 'ping' command to a Mongo database.
+ *
+ * @see <a href="https://www.mongodb.com/docs/manual/reference/command/ping/">mongo ping command</a>
  */
 @Slf4j
 public class MongoHealthCheck extends HealthCheck {
+
+    private static final String NO_ERRMSG_VALUE = "[No errmsg value]";
 
     /**
      * A default name that can be used when registering this health check.
@@ -22,39 +31,89 @@ public class MongoHealthCheck extends HealthCheck {
     @SuppressWarnings("unused")
     public static final String DEFAULT_NAME = "Mongo";
 
-    private final DB db;
+    private final MongoDatabase database;
+    private final String dbName;
 
-    public MongoHealthCheck(DB db) {
-        this.db = db;
+    public MongoHealthCheck(MongoDatabase database) {
+        this.database = database;
+        this.dbName = database.getName();
     }
 
-
-    @SuppressWarnings({"deprecation", "ConstantConditions"})
+    /**
+     * Checks the health of a Mongo database by issuing a Mongo ping command.
+     * <p>
+     * Results always include the value of the "ok" property in the details. When the ping
+     * command fails (without throwing an exception), the details will also include the
+     * following from the result: "errmsg", "code", "codeName".
+     * <p>
+     * For example, if you send an unknown command to Mongo, the "code" will be 59 and
+     * the "codeName" will be the string "CommandNotFound". Note also that "code" and
+     * "codeName" can be null. For a list of MongoDB error codes, see
+     * <a href="https://github.com/mongodb/mongo/blob/master/src/mongo/base/error_codes.yml">here</a>.
+     */
     @Override
     protected Result check() {
-        var host = db.getMongo().getAddress().toString();
-        var dbName = db.getName();
-
         try {
-            var statsResult = db.getStats();
+            var pingCommand = new BsonDocument("ping", new BsonInt32(1));
+            var commandResult = database.runCommand(pingCommand);
+            LOG.trace("ping result: {}", commandResult);
 
-            var isHealthy = statsResult.ok();
-            var resultBuilder = newResultBuilder(isHealthy)
-                    .withDetail("storageSize", statsResult.get("storageSize"))
-                    .withDetail("dataSize", statsResult.get("dataSize"));
+            var okValue = commandResult.get("ok");
+            var isHealthy = ok(okValue);
+            var resultBuilder = newResultBuilder(isHealthy).withDetail("ok", okValue);
 
             if (isHealthy) {
-                return resultBuilder.withMessage("Mongo %s/%s is up", host, dbName).build();
+                return resultBuilder
+                    .withMessage("Successfully pinged Mongo database %s", dbName)
+                    .build();
             }
 
+            var errorMessage = errorMessage(commandResult).orElse(NO_ERRMSG_VALUE);
             return resultBuilder
-                    .withMessage("Failed to retrieve db stats %s/%s : %s",
-                            host, dbName, statsResult.getErrorMessage())
-                    .build();
+                .withMessage("Error pinging Mongo database %s : %s", dbName, errorMessage)
+                .withDetail("code", commandResult.get("code"))
+                .withDetail("codeName", commandResult.get("codeName"))
+                .build();
+
         } catch (Exception e) {
-            LOG.error("Unable to connect to Mongo: {}/{}", host, dbName, e);
-            return newUnhealthyResult(HealthStatus.CRITICAL, "Unable to connect to Mongo %s/%s: %s",
-                    host, dbName, e.getMessage());
+            var message = f("Error pinging Mongo database {} ({}: {})",
+                    dbName, e.getClass().getName(), e.getMessage());
+            LOG.error(message, e);
+            return newUnhealthyResult(e, message);
         }
+    }
+
+    /**
+     * @implNote The old Mongo 3.x driver had a CommandResult class that encapsulated the
+     * logic to check if commands were successful. It checked whether the 'ok' property in the
+     * result Document was a boolean, which is why this method is checking for both numeric
+     * and boolean values. The Mongo docs say that commands always return a 0 or 1 (integer)
+     * as the value of the 'ok' property, but in reality it returns 0.0 (double), so I decided
+     * to leave the original logic to be conservative. However, since the result should be a
+     * number, this performs the check for a number first, whereas the CommandResult checked
+     * for a boolean first.
+     */
+    private static boolean ok(Object okValue) {
+        if (okValue instanceof Number) {
+            return ((Number) okValue).intValue() == 1;
+        } else if (okValue instanceof Boolean) {
+            return (boolean) okValue;
+        } else {
+            LOG.warn("Received unexpected {} value in ping result for 'ok': {}",
+                    classNameOrNull(okValue),okValue);
+            return false;
+        }
+    }
+
+    private static String classNameOrNull(Object value) {
+        return Optional.ofNullable(value)
+                .map(Object::getClass)
+                .map(Class::getName)
+                .orElse(null);
+    }
+
+    private static Optional<String> errorMessage(Document commandResult) {
+        var errmsgValue = commandResult.get("errmsg");
+        return Optional.ofNullable(errmsgValue).map(Object::toString);
     }
 }
