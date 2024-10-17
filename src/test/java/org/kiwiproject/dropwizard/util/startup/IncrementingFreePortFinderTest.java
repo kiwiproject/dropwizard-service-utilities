171b2b6701261b350c48248d946b6de3cc6a35bf
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-10-17 13:38:25 -0400

    Fix of-by-one error in IncrementingPortFinder (#553)
    Fixes #552

--- src/test/java/org/kiwiproject/dropwizard/util/startup/IncrementingFreePortFinderTest.java
+++ src/test/java/org/kiwiproject/dropwizard/util/startup/IncrementingFreePortFinderTest.java
@@ -101,6 +101,70 @@ class IncrementingFreePortFinderTest {
     }
 
     @Test
+    void shouldFindLastTwoPortsInRange_WhenAllOthersAreInUse() {
+        var checker = mock(LocalPortChecker.class);
+        var portFinder = new IncrementingFreePortFinder(checker);
+
+        when(checker.isPortAvailable(anyInt()))
+                .thenReturn(false)  // 9000
+                .thenReturn(false)  // 9001
+                .thenReturn(false)  // 9002
+                .thenReturn(false)  // 9003
+                .thenReturn(true)   // 9004
+                .thenReturn(true);  // 9005
+
+        var minPortNumber = 9_000;
+        var maxPortNumber = 9_005;
+
+        var servicePorts = portFinder.find(new AllowablePortRange(minPortNumber, maxPortNumber));
+
+        assertAll(
+                () -> assertThat(servicePorts.applicationPort()).isEqualTo(9_004),
+                () -> assertThat(servicePorts.adminPort()).isEqualTo(9_005)
+        );
+
+        verify(checker).isPortAvailable(9_000);
+        verify(checker).isPortAvailable(9_001);
+        verify(checker).isPortAvailable(9_002);
+        verify(checker).isPortAvailable(9_003);
+        verify(checker).isPortAvailable(9_004);
+        verify(checker).isPortAvailable(9_005);
+        verifyNoMoreInteractions(checker);
+    }
+
+    @Test
+    void shouldThrowNoAvailablePortException_WhenAllButLastPortAreInUse() {
+        var checker = mock(LocalPortChecker.class);
+        var portFinder = new IncrementingFreePortFinder(checker);
+
+        when(checker.isPortAvailable(anyInt()))
+                .thenReturn(false)  // 9000
+                .thenReturn(false)  // 9001
+                .thenReturn(false)  // 9002
+                .thenReturn(false)  // 9003
+                .thenReturn(false)  // 9004
+                .thenReturn(true);  // 9005
+
+        var minPortNumber = 9_000;
+        var maxPortNumber = 9_005;
+
+        var portRange = new AllowablePortRange(minPortNumber, maxPortNumber);
+
+        assertThatExceptionOfType(NoAvailablePortException.class)
+                .isThrownBy(() -> portFinder.find(portRange))
+                .withMessage("Could not find two open ports between %d and %d",
+                        minPortNumber, maxPortNumber);
+
+        verify(checker).isPortAvailable(9_000);
+        verify(checker).isPortAvailable(9_001);
+        verify(checker).isPortAvailable(9_002);
+        verify(checker).isPortAvailable(9_003);
+        verify(checker).isPortAvailable(9_004);
+        verify(checker).isPortAvailable(9_005);
+        verifyNoMoreInteractions(checker);
+    }
+
+    @Test
     void shouldThrowNoAvailablePortException_WhenNoPortsCanBeFound() {
         var checker = mock(LocalPortChecker.class);
         var portFinder = new IncrementingFreePortFinder(checker);
@@ -116,7 +180,7 @@ class IncrementingFreePortFinderTest {
                 .withMessage("Could not find two open ports between %d and %d",
                         minPortNumber, maxPortNumber);
 
-        var expectedNumberOfInvocations = maxPortNumber - minPortNumber;
+        var expectedNumberOfInvocations = portRange.getNumPortsInRange();
         verify(checker, times(expectedNumberOfInvocations)).isPortAvailable(anyInt());
         verifyNoMoreInteractions(checker);
     }
